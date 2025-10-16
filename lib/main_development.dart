338b2a5d1b7d5815bf01d5e26273d1eba089aaa0
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-16 18:39:06 +0300

    change the theme of the app

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Core/utils/constants.dart';
 import 'package:bookly/Features/Splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -7,7 +8,14 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(home: SplashView());
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor:
+            MyColors.kPrimaryColor,
+      ),
+      home: SplashView(),
+    );
   }
 }
 
