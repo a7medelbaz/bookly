af4416332df6d3cf224312b197c22ab1d5931bea
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-16 18:39:28 +0300

    editing the constants file

--- lib/Core/utils/constants.dart
+++ lib/Core/utils/constants.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 class MyColors {
-  static const kColor = Color(0xff100B20);
+  static const kPrimaryColor = Color(0xff100B20);
 }
 
-class MyImages {
-  static const booklyLogo =
+
+class MyAssets {
+    static const booklyLogo =
       'assets/images/Logo.png';
 }
-
-class MyAssets {}
