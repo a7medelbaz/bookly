a75c902d638f4990948d67ecddd1c35a1a4255ac
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-16 17:20:49 +0300

    adding my constant file

---
+++ lib/Core/utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class MyColors {
+  static const kColor = Color(0xff100B20);
+}
+
+class MyImages {
+  static const booklyLogo =
+      'assets/images/Logo.png';
+}
+
+class MyAssets {}
