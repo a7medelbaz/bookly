fcdc27a1e4f6043d43d9798679418936846ac7be
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-17 17:48:16 +0300

    comppelete the home view

--- lib/Features/Home/presentation/views/widgets/body_of_the_home_view.dart
+++ lib/Features/Home/presentation/views/widgets/body_of_the_home_view.dart
@@ -1,5 +1,6 @@
+import 'package:bookly/Core/utils/constants.dart';
+import 'package:bookly/Features/Home/presentation/views/widgets/books_list_view_builder.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/custom_abb_bar.dart';
-import 'package:bookly/Features/Home/presentation/views/widgets/custom_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BodyOfTheHomeView extends StatelessWidget {
@@ -10,8 +11,24 @@ class BodyOfTheHomeView extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        SizedBox(height: 30),
-        CustomBooksListView(),
+        SizedBox(height: 40),
+        BooksListViewBuilder(),
+        SizedBox(height: 40),
+        Padding(
+          padding: EdgeInsets.only(left: 15.0),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Best Sellers',
+              style: TextStyle(
+                fontFamily: MyAssets.kPrimaryFont,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+        ),
       ],
     );
   }
