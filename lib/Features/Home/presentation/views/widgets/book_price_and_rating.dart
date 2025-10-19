43140c05d9e455984e535ea715a5b80160405d36
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-19 18:42:14 +0300

    make BookPriceAndRating

---
+++ lib/Features/Home/presentation/views/widgets/book_price_and_rating.dart
@@ -0,0 +1,40 @@
+import 'package:bookly/Core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BookPriceAndRating extends StatelessWidget {
+  const BookPriceAndRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          '19.99 €',
+          style: MyStyles.textStyle18,
+        ),
+        SizedBox(width: 55),
+        Row(
+          mainAxisAlignment:
+              MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.star_rounded,
+              color: Colors.yellow,
+              size: 18,
+            ),
+            SizedBox(width: 6.3),
+            Text(
+              '4.8',
+              style: MyStyles.textStyle16,
+            ),
+            SizedBox(width: 9),
+            Text(
+              '(9999)',
+              style: MyStyles.textStyle14,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
