52ff5d50370a30dfc2ee0e644674800da0ba8245
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-20 19:30:42 +0300

    solve the cloumn e exciption

--- lib/Features/Home/presentation/views/widgets/book_deteils_card.dart
+++ lib/Features/Home/presentation/views/widgets/book_deteils_card.dart
@@ -8,27 +8,31 @@ class BookDeteilsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment:
-          CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width:
-              MediaQuery.of(context).size.width *
-              .5,
-          child: Text(
-            'A Dream of Spring - Game Of Thrones',
-            style: MyStyles.textStyle20,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
+    return Expanded(
+      child: Column(
+        crossAxisAlignment:
+            CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width:
+                MediaQuery.of(
+                  context,
+                ).size.width *
+                .5,
+            child: Text(
+              'A Dream of Spring - Game Of Thrones',
+              style: MyStyles.textStyle20,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
           ),
-        ),
-        Text(
-          'Gorge RR Martin',
-          style: MyStyles.textStyle14,
-        ),
-        BookPriceAndRating(),
-      ],
+          Text(
+            'Gorge RR Martin',
+            style: MyStyles.textStyle14,
+          ),
+          BookPriceAndRating(),
+        ],
+      ),
     );
   }
 }
