1c2d705b248216e510aa5840d9c23e2d2a5143e7
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-20 20:17:07 +0300

    rename the BestSellerBookBuilder to BestSellerBookAilverListBuilder and modify it to silverList and SliverChildBuilderDelegate

---
+++ lib/Features/Home/presentation/views/widgets/best_seller_book_ailver_list_builder.dart
@@ -0,0 +1,26 @@
+import 'package:bookly/Features/Home/presentation/views/widgets/best_seller_book_card.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerBookAilverListBuilder
+    extends StatelessWidget {
+  const BestSellerBookAilverListBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        childCount: 2,
+        (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10.0,
+            ),
+            child: BestSellerBookCard(),
+          );
+        },
+      ),
+    );
+  }
+}
