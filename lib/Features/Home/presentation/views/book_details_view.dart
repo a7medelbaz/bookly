9ca2a09ace7535100a3c7e6fc8717362d9509d4e
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-21 22:53:36 +0300

    start building BookDetailsView by build the appbar and the scaffold

--- lib/Features/Home/presentation/views/book_details_view.dart
+++ lib/Features/Home/presentation/views/book_details_view.dart
@@ -1,10 +1,32 @@
+import 'package:bookly/Features/Home/presentation/views/widgets/body_of_book_details_view.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BookDetailsView extends StatelessWidget {
   const BookDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            context.pop();
+          },
+          icon: Icon(Icons.close),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart_outlined,
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: BodyOfBookDetailsView(),
+      ),
+    );
   }
 }
