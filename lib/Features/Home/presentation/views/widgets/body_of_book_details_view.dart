f8c88dbfd258967eb9006edabb9e7a432d59cb49
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-22 12:04:01 +0300

    adding animation text to the BokkDetailsView

--- lib/Features/Home/presentation/views/widgets/body_of_book_details_view.dart
+++ lib/Features/Home/presentation/views/widgets/body_of_book_details_view.dart
@@ -1,10 +1,49 @@
+import 'package:bookly/Core/utils/styles.dart';
+import 'package:bookly/Features/Home/presentation/views/widgets/app_slide_animation_text.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/custom_book_card.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/selected_book_details.dart';
 import 'package:flutter/material.dart';
 
 class BodyOfBookDetailsView
-    extends StatelessWidget {
+    extends StatefulWidget {
   const BodyOfBookDetailsView({super.key});
+
+  @override
+  State<BodyOfBookDetailsView> createState() =>
+      _BodyOfBookDetailsViewState();
+}
+
+class _BodyOfBookDetailsViewState
+    extends State<BodyOfBookDetailsView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    // FutureDelay
+    // navigateToHomeView();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slideAnimation = Tween<Offset>(
+      begin: Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +60,18 @@ class BodyOfBookDetailsView
         ),
         SizedBox(height: 35),
         SelectedBookDetails(),
+        SizedBox(height: 50),
+        AppSlideAnimationText(
+          padding: EdgeInsets.only(left: 30.0),
+          alignment: Alignment.topLeft,
+          slideAnimation: slideAnimation,
+          text: 'You can also like',
+          textStyle: MyStyles.textStyle14
+              .copyWith(
+                fontStyle: FontStyle.italic,
+              ),
+        ),
+        SizedBox(height: 16),
       ],
     );
   }
