6371e39694c2aa91ff365b63b160f6bc0f563043
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-22 12:32:39 +0300

    finish the basics of the body

--- lib/Features/Home/presentation/views/widgets/books_recommendations_list.dart
+++ lib/Features/Home/presentation/views/widgets/books_recommendations_list.dart
@@ -1,10 +1,27 @@
+import 'package:bookly/Features/Home/presentation/views/widgets/custom_book_card.dart';
 import 'package:flutter/material.dart';
 
-class BooksRecommendationsList extends StatelessWidget {
+class BooksRecommendationsList
+    extends StatelessWidget {
   const BooksRecommendationsList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: 6,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(
+            right: 10.0,
+          ),
+          child: CustomBookCard(
+            aspectRatio: 2 / 3,
+            borderCircularRadius: 20,
+            horizontalPadding: 0,
+          ),
+        );
+      },
+    );
   }
 }
