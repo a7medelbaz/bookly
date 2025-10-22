d3143c62362b39e5a3e2ab13f934aa30731bd330
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-22 12:04:28 +0300

    start builing BooksRecommendationsList

---
+++ lib/Features/Home/presentation/views/widgets/books_recommendations_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BooksRecommendationsList extends StatelessWidget {
+  const BooksRecommendationsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
