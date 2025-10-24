32877e588d234bf8de4dc50d0a17a86ab73d8edc
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-24 14:33:23 +0300

    start impelment HomeRepo methodes

--- lib/Features/Home/data/repositories/home_repo_implementation.dart
+++ lib/Features/Home/data/repositories/home_repo_implementation.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Core/errors/my_failure.dart';
 import 'package:bookly/Core/utils/api_service.dart';
+import 'package:bookly/Core/utils/constants.dart';
 import 'package:bookly/Features/Home/data/models/book_model.dart';
 import 'package:bookly/Features/Home/data/repositories/home_repo.dart';
 import 'package:dartz/dartz.dart';
@@ -8,14 +9,31 @@ class HomeRepoImplementation implements HomeRepo {
   final ApiService apiService;
   HomeRepoImplementation(this.apiService);
   @override
-  Future<Either<MyFailure, BookModel>>
-  featchBestSellereBooks() {
-    // TODO: implement featchBestSellereBooks
-    throw UnimplementedError();
+  Future<Either<MyFailure, List<BookModel>>>
+  featchBestSellereBooks() async {
+    try {
+      List<dynamic> listOfBestSellerBooks =
+          await apiService.get(
+            endPoint:
+                MyUrlConstants.bestSellerEndPoint,
+            token: MyUrlConstants.myBookToken,
+          );
+      // List<BookModel> bestSellerBooks = [];
+      return right(
+        listOfBestSellerBooks
+            // Iterable of maps of listOfBestSellerBooks
+            .map(
+              (book) => BookModel.fromJson(book),
+            )
+            .toList(),
+      );
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<MyFailure, BookModel>>
+  Future<Either<MyFailure, List<BookModel>>>
   featchFeatureBooks() {
     // TODO: implement featchFeatureBooks
     throw UnimplementedError();
