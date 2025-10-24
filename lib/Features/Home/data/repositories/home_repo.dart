1298c3171b7bb27ea0fdde1fe6eefd3e44f29a4e
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-24 10:27:11 +0300

    Refactor HomeRepo to include feature and best seller book fetching methods

--- lib/Features/Home/data/repositories/home_repo.dart
+++ lib/Features/Home/data/repositories/home_repo.dart
@@ -1,4 +1,12 @@
+import 'package:bookly/Core/errors/my_failure.dart';
+import 'package:bookly/Features/Home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
+  // Feature Books
+  Future<Either<MyFailure, BookModel>>
   featchFeatureBooks();
+  // Best Seller Books
+  Future<Either<MyFailure, BookModel>>
   featchBestSellereBooks();
 }
