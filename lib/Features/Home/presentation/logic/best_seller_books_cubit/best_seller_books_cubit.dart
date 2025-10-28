22d9de5374137c62cba0f2d1be9886d99f90b3c8
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-28 22:41:35 +0300

    create cubit for bestseller books

--- lib/Features/Home/presentation/logic/best_seller_books_cubit/best_seller_books_cubit.dart
+++ lib/Features/Home/presentation/logic/best_seller_books_cubit/best_seller_books_cubit.dart
@@ -1,8 +1,34 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/Home/data/models/book_model.dart';
+import 'package:bookly/Features/Home/data/repositories/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'best_seller_books_state.dart';
 
-class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
-  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+class BestSellerBooksCubit
+    extends Cubit<BestSellerBooksState> {
+  BestSellerBooksCubit(this.homeRepo)
+    : super(BestSellerBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future featchGeneralBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo
+        .featchBestSellereBooks();
+
+    result.fold(
+      (failure) {
+        emit(
+          BestSellerBooksFailure(
+            errorMassage: failure.errorMassage,
+          ),
+        );
+      },
+      (myBooks) {
+        emit(
+          BestSellerBooksSuccess(books: myBooks),
+        );
+      },
+    );
+  }
 }
