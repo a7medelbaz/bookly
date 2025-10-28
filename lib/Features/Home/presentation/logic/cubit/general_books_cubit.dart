4d4ff0288a9caea23fbe21ead3244ec96fb2105c
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-28 22:22:00 +0300

    create cubit and state

--- lib/Features/Home/presentation/logic/cubit/general_books_cubit.dart
+++ lib/Features/Home/presentation/logic/cubit/general_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/Home/data/models/book_model.dart';
 import 'package:equatable/equatable.dart';
 
 part 'general_books_state.dart';
--- lib/Features/Home/presentation/logic/cubit/general_books_state.dart
+++ lib/Features/Home/presentation/logic/cubit/general_books_state.dart
@@ -7,4 +7,26 @@ sealed class GeneralBooksState extends Equatable {
   List<Object> get props => [];
 }
 
-final class GeneralBooksInitial extends GeneralBooksState {}
+final class GeneralBooksInitial
+    extends GeneralBooksState {}
+
+final class GeneralBooksLoading
+    extends GeneralBooksState {}
+
+final class GeneralBooksFailure
+    extends GeneralBooksState {
+  final String errorMassage;
+
+  const GeneralBooksFailure({
+    required this.errorMassage,
+  });
+}
+
+final class GeneralBooksSuccess
+    extends GeneralBooksState {
+  final List<BookModel> books;
+
+  const GeneralBooksSuccess({
+    required this.books,
+  });
+}
