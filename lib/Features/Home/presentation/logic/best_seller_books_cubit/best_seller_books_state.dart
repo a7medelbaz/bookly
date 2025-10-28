9a5854680b1174e5005af607c83b0267437a51a6
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-28 22:41:17 +0300

    create state for best seller

--- lib/Features/Home/presentation/logic/best_seller_books_cubit/best_seller_books_state.dart
+++ lib/Features/Home/presentation/logic/best_seller_books_cubit/best_seller_books_state.dart
@@ -1,10 +1,32 @@
 part of 'best_seller_books_cubit.dart';
 
-sealed class BestSellerBooksState extends Equatable {
+sealed class BestSellerBooksState
+    extends Equatable {
   const BestSellerBooksState();
 
   @override
   List<Object> get props => [];
 }
 
-final class BestSellerBooksInitial extends BestSellerBooksState {}
+final class BestSellerBooksInitial
+    extends BestSellerBooksState {}
+
+final class BestSellerBooksLoading
+    extends BestSellerBooksState {}
+
+final class BestSellerBooksFailure
+    extends BestSellerBooksState {
+  final String errorMassage;
+
+  const BestSellerBooksFailure({
+    required this.errorMassage,
+  });
+}
+
+final class BestSellerBooksSuccess
+    extends BestSellerBooksState {
+  final List<BookModel> books;
+  const BestSellerBooksSuccess({
+    required this.books,
+  });
+}
