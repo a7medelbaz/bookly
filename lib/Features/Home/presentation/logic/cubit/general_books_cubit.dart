f70b0167813a335f2ef338a9944ddf6c4dbd81a5
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-28 22:33:37 +0300

    creating featchGeneralBooks methode

--- lib/Features/Home/presentation/logic/cubit/general_books_cubit.dart
+++ lib/Features/Home/presentation/logic/cubit/general_books_cubit.dart
@@ -1,9 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/Home/data/models/book_model.dart';
+import 'package:bookly/Features/Home/data/repositories/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'general_books_state.dart';
 
-class GeneralBooksCubit extends Cubit<GeneralBooksState> {
-  GeneralBooksCubit() : super(GeneralBooksInitial());
+class GeneralBooksCubit
+    extends Cubit<GeneralBooksState> {
+  GeneralBooksCubit(this.homeRepo)
+    : super(GeneralBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future featchGeneralBooks() async {
+    emit(GeneralBooksLoading());
+    var result = await homeRepo
+        .featchGeneraleBooks();
+
+    result.fold(
+      (failure) {
+        emit(
+          GeneralBooksFailure(
+            errorMassage: failure.errorMassage,
+          ),
+        );
+      },
+      (myBooks) {
+        emit(GeneralBooksSuccess(books: myBooks));
+      },
+    );
+  }
 }
