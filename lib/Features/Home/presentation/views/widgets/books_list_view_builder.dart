68db20ce2ab4081abbc24516e086576bc0de02fa
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-30 18:36:45 +0300

    start displaying the data by building bloc builder

--- lib/Features/Home/presentation/views/widgets/books_list_view_builder.dart
+++ lib/Features/Home/presentation/views/widgets/books_list_view_builder.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/Core/widgets/custom_error_widget.dart';
+import 'package:bookly/Core/widgets/custom_loading_indicator.dart';
+import 'package:bookly/Features/Home/presentation/logic/general_books_cubit/general_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'custom_book_card.dart';
 
@@ -7,19 +11,29 @@ class BooksListViewBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .28,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 4,
-        itemBuilder: (context, index) {
-          return CustomBookCard(
-            horizontalPadding: 15,
-            borderCircularRadius: 20,
-            aspectRatio: 3 / 4,
+    return BlocBuilder<GeneralBooksCubit, GeneralBooksState>(
+      builder: (context, state) {
+        if (state is GeneralBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .28,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return CustomBookCard(
+                  horizontalPadding: 15,
+                  borderCircularRadius: 20,
+                  aspectRatio: 3 / 4,
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is GeneralBooksFailure) {
+          return CustomErrorWidget(errorMessage: state.errorMassage);
+        } else {
+          return CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
